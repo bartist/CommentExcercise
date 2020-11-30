deccdbda67aa8cb5336bf3db05e913eb670758e0
Bart <de.uil.met.een@gmail.com>
2020-11-30 08:27:17 +0100

    Create Code5.py

---
+++ Code5.py
@@ -0,0 +1,7 @@
+test_str = 'Computer_science_is_best'
+
+print("The original string is : " + test_str) 
+
+res = test_str.replace("_", " ").title().replace(" ", "") 
+  
+print("The String after changing case : " + str(res)) 
